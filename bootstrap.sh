a26900662fdabb25b53961d6a91e977c5970649b
somebody <noreply@example.org>
2019-06-28 11:28:14 +0800

    Set DNS server and install some tools

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,16 @@
 # Add the code to Vagrantfile
 # config.vm.provision :shell, path: "bootstrap.sh"
 
+sudo su -
+
+# Set timezone
+echo "Asia/Shanghai" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+# Set DNS Server
+echo "nameserver 223.5.5.5" > /etc/resolv.conf
+echo "nameserver 114.114.114.114" >> /etc/resolv.conf
+
 # change source mirror
 cd /etc/apt/
 mv sources.list sources.list.bak
@@ -11,4 +21,4 @@ apt update -y
 apt upgrade -y
 
 # install nginx & git
-apt install -y nginx git
+apt install -y rsync wget vim git
